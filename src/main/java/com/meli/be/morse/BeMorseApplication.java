125f3720976ccc24bc6cbdd87d9d11bfb8de104d
Mauricio <mrsanchez@baufest.com.ar>
2020-05-03 17:34:46 -0300

    firts commit appi

---
+++ src/main/java/com/meli/be/morse/BeMorseApplication.java
@@ -0,0 +1,13 @@
+package com.meli.be.morse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMorseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/be/morse/BeMorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.be.morse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
